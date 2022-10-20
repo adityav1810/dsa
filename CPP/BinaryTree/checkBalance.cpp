f825b1e88841ed2d128af02ebaed4a99ba5897dc
adityav1810 <adityavarshney99@gmail.com>
2022-10-20 23:39:01 +0530

    BinaryTree

--- CPP/BinaryTree/checkBalance.cpp
+++ CPP/BinaryTree/checkBalance.cpp
@@ -34,3 +34,30 @@ public:
         }
     }
 };
+
+
+// Linear time solution by calculating balance and height in 1 pass, simlar to diameter of btree soln
+    pair<bool,int>getdia_and_height(TreeNode *root){
+        if(root == NULL){
+            return make_pair(true,0);
+        }
+        pair<bool,int> left = getdia_and_height(root->left);
+        pair<bool,int> right = getdia_and_height (root->right);
+        
+        int d = abs(left.second - right.second);
+        
+        pair<bool,int> ans;
+        
+        ans.second = max(left.second , right.second)+1;
+        if((d <=1) && (left.first == true && right.first == true)){
+            ans.first = true;
+        }else{
+            ans.first = false;
+        }
+        return ans;
+    }
+    
+    
+    bool isBalanced(TreeNode* root) {
+        return getdia_and_height(root).first;
+    }
