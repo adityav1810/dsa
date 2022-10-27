b1466e5945e033e4e79c345f5abc651e18d00ff8
adityav1810 <adityavarshney99@gmail.com>
2022-10-27 22:19:54 +0530

    View Binary Tree

---
+++ CPP/BinaryTree/levelOrder.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<TreeNode *> q;
+        vector<vector<int>> res;
+        if(root == NULL){
+            return res;
+        }
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> tmp;
+            while(size --){
+                TreeNode *frontNode = q.front();
+            q.pop();
+            tmp.push_back(frontNode->val);
+            if(frontNode->left){
+                q.push(frontNode->left);
+            }
+            if(frontNode->right){
+                q.push(frontNode->right);
+            }
+            }
+            
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
